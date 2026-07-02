e6b35eb96f459d8e5787114ed265844907616186
SDorGS <160716052+SDorGS@users.noreply.github.com>
2026-07-02 14:40:37 +0100

    Update Order.ts

--- frontend/models/Order.ts
+++ frontend/models/Order.ts
@@ -1,18 +1,18 @@
-
 import { Order as OrderType } from "@/types/order";
 
 export default class Order implements OrderType {
-  id: string;
-  listingId: string;
-  buyerId: string;
-  sellerId: string;
-  status: OrderType["status"];
+  id!: string;
+  listingId!: string;
+  buyerId!: string;
+  sellerId!: string;
+
+  status!: OrderType["status"];
 
   constructor(data: OrderType) {
     Object.assign(this, data);
   }
 
-  clone() {
+  clone(): Order {
     return new Order({
       ...this,
     });
