e14cd1715e0173d147d54b2f1dd735014de6b9fd
SDorGS <160716052+SDorGS@users.noreply.github.com>
2026-07-02 14:42:09 +0100

    Refactor User class properties to use definite assignment

--- frontend/models/User.ts
+++ frontend/models/User.ts
@@ -1,24 +1,20 @@
-
 import { User as UserType } from "@/types/user";
 
 export default class User implements UserType {
-  id: string;
-  name: string;
-  email: string;
+  id!: string;
+  name!: string;
+  email!: string;
   avatar?: string;
-  role: UserType["role"];
-  verified: boolean;
+  role!: UserType["role"];
+  verified!: boolean;
 
   constructor(data: UserType) {
     Object.assign(this, data);
   }
 
-  clone() {
+  clone(): User {
     return new User({
       ...this,
     });
   }
 }
-
-
-====================================================================================================
